6d245d560316baf46b01746a5f8304d1e7013b5f
kanchan kapri <kaprikanchan09@gmail.com>
2026-09-10 21:02:17 +0530

    day 1 task 2

---
+++ Day1/Task2/Kanchan.java
@@ -0,0 +1,30 @@
+// import java.util.*;
+
+public class Kanchan {
+
+    public static void Jadu ( int n ) {
+        for( int i = 1; i<=n; i++) {
+            for( int j = 1; j <= n-i; j++) {
+                System.out.print(
+                    " "
+                );
+            }
+
+            for( int k = 1; k <= 2*i-1; k++) {
+                System.out.print("*");
+            }
+        
+
+            System.out.println(" ");
+        }
+    }
+
+
+    
+    public static void main ( String args[]) {
+        int n = 4;
+        // Kanchan obj = new Kanchan();
+        Kanchan.Jadu(n);
+    }
+
+}
